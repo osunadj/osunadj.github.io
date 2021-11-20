ad36444c59a2f9e039b282523e5b6b3f6afc37d0
osunadj <osunadjessica1@gmail.com>
2021-11-19 19:25:48 -0600

    Cambios y subida de un chat al ejercicio de autentificacion

---
+++ README.md
@@ -0,0 +1,2 @@
+# rdwp801.github.io
+Ejemplo de un chat y acceso a Firebase (Firestore con Google)
---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBzWLGJUu_t1Nj5Mlwi9kMAFzkhjQQj6-0",
+  authDomain: "autenticacion801.firebaseapp.com",
+  projectId: "autenticacion801",
+  storageBucket: "autenticacion801.appspot.com",
+  messagingSenderId: "935072842791",
+  appId: "1:935072842791:web:9656c801cc6e63f818fa02",
+  measurementId: "G-EFNDG7JT21"
+    });
